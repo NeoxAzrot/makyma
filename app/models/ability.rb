db789c29e0c4c98fbac7afb05b6be7c83925a07a
Hugofgx9 <hugo.faugeroux@mmibordeaux.com>
2020-02-28 11:56:38 +0100

    new db and user ok

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,12 +5,13 @@ class Ability
 
 
   def initialize(user)
-
-    if user.admin_role?
-        can :manage, :all
-    else
-        can :read, :all
-    end
+    if user
+        if user.admin_role?
+            can :manage, :all
+        else
+            can :read, :all
+        end
+    end 
 
 
 
