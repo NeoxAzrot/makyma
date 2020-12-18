f44f595e1bd5d0cf92aae03b9256359507ee5425
Hugofgx9 <hugo.faugeroux@mmibordeaux.com>
2020-12-18 09:58:32 +0100

    multiple words search engine

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
 
 	def index
-    @search = params[:id].downcase
+    @params = params[:id].parameterize.split('-')
     @nbOfResults = 0
+    searchItems = Array.new
 
     #fill product dico
     dico = Array.new
@@ -22,14 +23,27 @@ class SearchController < ApplicationController
     end
     dico.sort!
 
-    #search search in dico
-    spell_checker = DidYouMean::SpellChecker.new(dictionary: dico)
-    dicoResult = spell_checker.correct(@search).last
+    #fill array of each search word
+    @params.each do |word|
 
-    searchItems = [@search, dicoResult]
+      #search exact word in dico and add it to searchItems
+      if word.in?(dico)
+        searchItems.push(word)
+      end
+
+      #search similar word in dico and add it to searchItems
+      spell_checker = DidYouMean::SpellChecker.new(dictionary: dico)
+      if spell_checker.correct(word).present?
+        dicoResult = spell_checker.correct(word).last
+        searchItems.push(dicoResult)
+      end
+
+    end
+
+    @params = searchItems
 
     #query the dico result
-    if @search.present?
+    if @params.present?
       @products = Array.new
       @alternatives = Array.new
 
@@ -38,7 +52,17 @@ class SearchController < ApplicationController
         @alternatives += Alternative.where("lower(title) LIKE :search OR lower(description) LIKE :search", search: "%#{item}%")
       end
 
-      if !@products.present? && !@alternatives.present?
+      def has_already?(array)
+         h = (Thread.current[:_has_three_buffer] ||= Hash.new(0)).clear
+         # h = Hash.new(0)
+         array.each do |i|
+            h[i] += 1
+            return true if h[i] >= 2
+         end
+         false
+      end
+
+      if @products.empty? && @alternatives.empty?
       	nbOfProducts = Product.count
       	nbOfSuggestions = 3
       	@suggestions = Array.new(nbOfSuggestions)
