fd9d4d69b58a36565f136a6e5fee128f74639fed
EliiiseE <elise.echasseriau@mmibordeaux.com>
2021-03-02 18:46:21 +0100

    presse

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
   match '/about', to: 'pages#about', via: 'get'
   match '/cgu', to: 'pages#cgu', via: 'get'
   match '/contact', to: 'pages#contact', via: 'get'
+  match '/presse', to: 'pages#presse', via: 'get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
