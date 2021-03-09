0e28e2f7d921882e76d9365bc37d706f56dd7b40
Hugofgx9 <hugo.faugeroux@mmibordeaux.com>
2021-03-09 15:50:15 +0100

    add breadcrumb to simple pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,25 @@ class PagesController < ApplicationController
 	def show
 		render template: "pages/#{params[:page]}"
 	end
+	
+	def about 
+		add_breadcrumb 'Accueil', :root_path
+		add_breadcrumb 'À propos', 'about'
+	end
+
+	def cgu 
+		add_breadcrumb 'Accueil', :root_path
+		add_breadcrumb 'Mentions Légales', 'cgu'
+	end
+
+	def presse 
+		add_breadcrumb 'Accueil', :root_path
+		add_breadcrumb 'Nos ressources presse', 'Presse'
+	end
+
+	def suggestion 
+		add_breadcrumb 'Accueil', :root_path
+		add_breadcrumb 'Proposer une alternative', 'suggestion'
+	end
 
 end
