7a026d744f032788d71df409ed6278996951ccaa
NeoxAzrot <sami.lepays@gmail.com>
2021-03-16 18:40:49 +0100

    mail try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,8 +93,7 @@ Rails.application.configure do
 end
 
   # Mail
-
-  config.action_mailer.perform_caching = false
+  #config.action_mailer.perform_caching = false
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
