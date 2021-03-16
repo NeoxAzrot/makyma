b7c6b0fed662654c8f33f6d66bd80bc073ab0f67
NeoxAzrot <sami.lepays@gmail.com>
2021-03-16 18:10:57 +0100

    mail

--- app/mailers/makyma_notice_mailer.rb
+++ app/mailers/makyma_notice_mailer.rb
@@ -1,6 +1,6 @@
 class MakymaNoticeMailer < ApplicationMailer
 
   def suggested_alternative_email
-    mail(to: 'makymaorg@gmail.com', subject: 'New Suggested Alternative')
+    mail(to: 'makymaorg@gmail.com', subject: 'Nouvelle alternative suggérée !')
   end
 end
