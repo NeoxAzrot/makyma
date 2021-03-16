7dc93925a281befd6ae55152d172bbab86397b10
NeoxAzrot <sami.lepays@gmail.com>
2021-03-16 18:42:25 +0100

    mail try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,16 +81,6 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-  
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
-  
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-end
 
   # Mail
   #config.action_mailer.perform_caching = false
@@ -113,3 +103,13 @@ end
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
+  
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+  
+  # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
+end
